a4604b78618d5404f6a8db55c5c234bf03863719
STEBAN CADENA GIRALDO <stebancg@s2pc28.eisc.univalle.edu.co>
2016-12-02 08:04:51 -0500

    hola

--- main.cpp
+++ main.cpp
@@ -7,6 +7,15 @@ int main(int argc, char * argv[]){
 	
 	string nombre, sexo;
 	int cedula, edad;
+
 	cout << "Ingrese los dfatos del paciente: " << endl;
+	cout << "Ingrese el numero de identificación:" << endl;
+	cin >> cedula;
+	cout << "Ingrese el nombre del paciente: " << endl;
+	cin >> nombre;
+	cout << "Ingrese la edad del paciente: " << endl;
+	cin >> edad;
+	cout << "Ingrese el sexo del paciente: " << endl;
+	cin >> sexo;
 
 }
